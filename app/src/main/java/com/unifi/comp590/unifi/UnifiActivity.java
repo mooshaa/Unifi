832d64933853fa67f44a29ce347d8acb68e51d89
Moosa <moosag123@gmail.com>
2018-11-06 15:01:36 -0800

    Initialize

---
+++ app/src/main/java/com/unifi/comp590/unifi/UnifiActivity.java
@@ -0,0 +1,13 @@
+package com.unifi.comp590.unifi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnifiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unifi);
+    }
+}
