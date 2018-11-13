830fff98d688fdff47f376b49aca05e71c09fcb2
Moosa <moosag123@gmail.com>
2018-11-13 07:07:28 -0800

    retrieve data from database

--- app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
+++ app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
@@ -1,13 +1,70 @@
 package com.unifi.comp590.unifi;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private CircleImageView settingsImage;
+    private EditText settingsName;
+    private EditText settingsStatus;
+    private Button settingsSave;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mAuth = FirebaseAuth.getInstance();
+        String user_id = mAuth.getCurrentUser().getUid();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+
+
+        settingsImage = (CircleImageView) findViewById(R.id.account_settings_user_image);
+        settingsName = (EditText) findViewById(R.id.account_settings_username);
+        settingsStatus = (EditText) findViewById(R.id.account_settings_status);
+        settingsSave = (Button) findViewById(R.id.account_settings_button_save);
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("user_name").getValue().toString();
+                String status = dataSnapshot.child("user_status").getValue().toString();
+                String image = dataSnapshot.child("user_image").getValue().toString();
+                String thumbnail = dataSnapshot.child("user_thumbnail").getValue().toString();
+                settingsName.setText(name);
+                settingsStatus.setText(status);
+//                settingsImage.setImageBitmap();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        settingsSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = settingsName.getText().toString();
+                String status = settingsStatus.getText().toString();
+            }
+        });
     }
 }
