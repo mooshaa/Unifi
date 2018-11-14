81b67a72a6c55e3f4363076c16891d4edbf91e79
Moosa <moosag123@gmail.com>
2018-11-13 18:56:16 -0800

    change pic

--- app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
+++ app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.unifi.comp590.unifi;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -25,6 +29,9 @@ public class SettingsActivity extends AppCompatActivity {
     private Button settingsSave;
     private DatabaseReference mDatabaseReference;
     private FirebaseAuth mAuth;
+    private final static int GALLERY_CODE=1;
+    private ImageButton changeImageOpaque;
+    private ImageButton changeImageTransparent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +42,20 @@ public class SettingsActivity extends AppCompatActivity {
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
 
 
-
+        changeImageOpaque = (ImageButton) findViewById(R.id.settings_image_edit_button_opaque);
+        changeImageTransparent = (ImageButton) findViewById(R.id.settings_image_edit_button_transparent);
+//   TODO     changeImageTransparent.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View view, boolean b) {
+//                switch (motionEvent.getAction()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                        changeImageTransparent.setVisibility(View.GONE);
+//                        changeImageOpaque.setVisibility(View.VISIBLE);
+//                    case MotionEvent.ACTION_UP:
+//                        changeImageTransparent.setVisibility(View.VISIBLE);
+//                        changeImageOpaque.setVisibility(View.GONE);
+//            }
+//        })
         settingsImage = (CircleImageView) findViewById(R.id.account_settings_user_image);
         settingsName = (EditText) findViewById(R.id.account_settings_username);
         settingsStatus = (EditText) findViewById(R.id.account_settings_status);
@@ -55,6 +75,17 @@ public class SettingsActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        settingsImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_CODE);
 
             }
         });
