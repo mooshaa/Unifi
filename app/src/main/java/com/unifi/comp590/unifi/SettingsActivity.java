3413f5b4d7214ffd30947d1ef114b526e7297b18
Moosa <moosag123@gmail.com>
2018-11-14 00:42:25 -0800

    added cropping dependecy and permissions

--- app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
+++ app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.unifi.comp590.unifi;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -97,5 +98,13 @@ public class SettingsActivity extends AppCompatActivity {
                 String status = settingsStatus.getText().toString();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_CODE && requestCode == RESULT_OK && data != null) {
+
+        }
     }
 }
