3d9ee8b7002dea97a336ef9366f892ac33034fff
Moosa <moosag123@gmail.com>
2018-11-15 08:53:25 -0800

    thumbnails issue with user_image entry

--- app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
+++ app/src/main/java/com/unifi/comp590/unifi/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.unifi.comp590.unifi;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,12 +9,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -31,9 +30,14 @@ import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -48,6 +52,9 @@ public class SettingsActivity extends AppCompatActivity {
     private final static int GALLERY_CODE=1;
     private ImageButton changeImageOpaque;
     private ImageButton changeImageTransparent;
+    private Bitmap thumbnailbitmap;
+    private StorageReference thumbreference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +64,7 @@ public class SettingsActivity extends AppCompatActivity {
         String user_id = mAuth.getCurrentUser().getUid();
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
         mImageStorageReference = FirebaseStorage.getInstance().getReference().child("Profile_Image");
+        thumbreference = FirebaseStorage.getInstance().getReference().child("Thumbnails");
 
 
         changeImageOpaque = (ImageButton) findViewById(R.id.settings_image_edit_button_opaque);
@@ -142,7 +150,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GALLERY_CODE && resultCode == RESULT_OK && data != null) {
             Uri imageUri = data.getData();
             CropImage.activity(imageUri).setGuidelines(CropImageView.Guidelines.ON).setAspectRatio(1, 1).start(this);
@@ -152,23 +160,49 @@ public class SettingsActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 Uri resultUri = result.getUri();
 
-                File thumbnaillocation = new File(resultUri.getPath());
-
+                File thumbUri = new File(resultUri.getPath());
 
                 String user_id = mAuth.getCurrentUser().getUid();
+
+                try {
+                    thumbnailbitmap = new Compressor(this).setMaxHeight(200).setMaxWidth(200).setQuality(50)
+                            .compressToBitmap(thumbUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                thumbnailbitmap.compress(Bitmap.CompressFormat.JPEG,50,byteArrayOutputStream);
+                final byte[] thumbByte=byteArrayOutputStream.toByteArray();
+
                 StorageReference filePath = mImageStorageReference.child(user_id+".jpg");
+                final StorageReference thumbPath = thumbreference.child(user_id + ".jpg");
+
                 filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
                         if (task.isSuccessful()) {
-                            String dlURL = task.getResult().getStorage().getDownloadUrl().toString();
-                            mDatabaseReference.child("user_image").setValue(dlURL).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            final String dlURL = task.getResult().getStorage().getDownloadUrl().toString();
+                            UploadTask uploadTask = thumbPath.putBytes(thumbByte);
+                            uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                                 @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    Toast.makeText(SettingsActivity.this, "Profile picture updated.", Toast.LENGTH_LONG).show();
+                                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                    String thumbdlURL = task.getResult().getStorage().getDownloadUrl().toString();
+                                    if (task.isSuccessful()) {
+                                        Map user_images = new HashMap();
+                                        user_images.put("user_image", dlURL);
+                                        user_images.put("user_thumbnail",thumbdlURL);
+                                        mDatabaseReference.updateChildren(user_images).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                Toast.makeText(SettingsActivity.this, "Profile picture updated.", Toast.LENGTH_LONG).show();
 
+                                            }
+                                        });
+                                    }
                                 }
                             });
+
+
                         } else {
                             Toast.makeText(SettingsActivity.this, "Error while updating picture", Toast.LENGTH_LONG).show();
 
